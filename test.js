0052c16b18743f800e11da68828ddec6dc3ba8a1
Rashmi Gupta <rashmi224141@gmail.com>
2020-03-01 18:32:16 +0530

    added test

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('test', function() {
+  it('test', function() {
+    let actual = 5;
+    let expected = 5;
+    assert.strictEqual(actual, expected);
+  });
+});
