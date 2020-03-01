ff108abe993fc7d8a5eaa46abb99a6db808c9a1d
Rashmi Gupta <rashmi224141@gmail.com>
2020-03-01 18:41:11 +0530

    added  some more test

--- test.js
+++ test.js
@@ -7,3 +7,10 @@ describe('test', function() {
     assert.strictEqual(actual, expected);
   });
 });
+describe('just for test', function() {
+  it('just for test', function() {
+    let actual = 5;
+    let expected = 5;
+    assert.strictEqual(actual, expected);
+  });
+});
