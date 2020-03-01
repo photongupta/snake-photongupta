117bdd8d210ba425ca2059e66511fc2d8d5344fe
Rashmi Gupta <rashmi224141@gmail.com>
2020-03-01 18:53:57 +0530

    removed test

--- test.js
+++ test.js
@@ -7,10 +7,3 @@ describe('test', function() {
     assert.strictEqual(actual, expected);
   });
 });
-describe('just for test', function() {
-  it('just for test', function() {
-    let actual = 5;
-    let expected = 5;
-    assert.strictEqual(actual, expected);
-  });
-});
